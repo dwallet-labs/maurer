4c888b9c86f849d50737699b096871d00b7442a0
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-05 00:33:59 +0200

    Update src/discrete_log_ratio_of_committed_values.rs
    Co-authored-by: dolevmu <56167459+dolevmu@users.noreply.github.com>

--- src/discrete_log_ratio_of_committed_values.rs
+++ src/discrete_log_ratio_of_committed_values.rs
@@ -12,6 +12,8 @@ use crate::Result;
 use crate::SOUND_PROOFS_REPETITIONS;
 
 /// Ratio Between Committed Values is the Discrete Log Maurer Language.
+/// $$ (m,r_1,r_2) \mapsto Com_{G,H}(m; r_1), Com_{X,H}(m, r_2) $$
+/// Where $X=g^x$ is a public parameter and we use Pedersen commitments where $Com_{X,H}(m,r_2) = Com_{G,H}(x*m, r_2)$.
 ///
 /// SECURITY NOTICE:
 /// Because correctness and zero-knowledge is guaranteed for any group in this language, we choose
