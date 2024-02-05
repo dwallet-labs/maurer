b07cb7e2db3c3ee4691eb0ad10f4c7fbff205b73
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-05 02:14:27 +0200

    take updates

--- src/aggregation/decommitment_round.rs
+++ src/aggregation/decommitment_round.rs
@@ -1,15 +1,17 @@
 // Author: dWallet Labs, Ltd.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use commitment::Commitment;
 use crypto_bigint::rand_core::CryptoRngCore;
 use group::{ComputationalSecuritySizedNumber, GroupElement, PartyID};
 use proof::aggregation::DecommitmentRoundParty;
 use serde::{Deserialize, Serialize};
 
+use commitment::Commitment;
+
 use crate::{Error, Result};
+use crate::aggregation::{process_incoming_messages, proof_share_round};
 use crate::language;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
@@ -20,7 +22,7 @@ pub struct Decommitment<const REPETITIONS: usize, Language: language::Language<R
     pub(super) commitment_randomness: ComputationalSecuritySizedNumber,
 }
 
-#[cfg_attr(feature = "benchmarking", derive(Clone))]
+#[cfg_attr(feature = "test_helpers", derive(Clone))]
 pub struct Party<
     // Number of times this proof should be repeated to achieve sufficient security
     const REPETITIONS: usize,
@@ -32,15 +34,14 @@ pub struct Party<
     ProtocolContext: Clone,
 > {
     pub(super) party_id: PartyID,
-    pub(super) threshold: PartyID,
-    pub(super) number_of_parties: PartyID,
+    pub(crate) provers: HashSet<PartyID>,
     pub(super) language_public_parameters: Language::PublicParameters,
     pub(super) protocol_context: ProtocolContext,
     pub(super) witnesses: Vec<Language::WitnessSpaceGroupElement>,
     pub(super) statements: Vec<Language::StatementSpaceGroupElement>,
     pub(super) randomizers: [Language::WitnessSpaceGroupElement; REPETITIONS],
     pub(super) statement_masks: [Language::StatementSpaceGroupElement; REPETITIONS],
-    pub(super) commitment_randomness: ComputationalSecuritySizedNumber,
+    pub(super) decommitment: Decommitment::<REPETITIONS, Language>,
 }
 
 
@@ -59,38 +60,14 @@ for Party<REPETITIONS, Language, ProtocolContext>
     fn decommit_statements_and_statement_mask(
         self,
         commitments: HashMap<PartyID, Self::Commitment>,
-        rng: &mut impl CryptoRngCore,
+        _rng: &mut impl CryptoRngCore,
     ) -> Result<(Self::Decommitment, Self::ProofShareRoundParty)> {
-        let commitments: HashMap<_, _> = commitments
-            .into_iter()
-            .filter(|(party_id, _)| *party_id != self.party_id)
-            .collect();
-
-        // TODO: is this sufficient? later rounds check against the same party set so this should
-        // cover that. TODO: test this
-        // if commitments.len() + 1 < self.threshold.into() {
-        //     return Err(Error::ThresholdNotReached)?;
-        // }
-
-        let decommitment = Decommitment::<REPETITIONS, Language> {
-            statements: self
-                .statements
-                .iter()
-                .map(|statement| statement.value())
-                .collect(),
-            // TODO: take this from previous round instead of computing values again here.
-            statement_masks: self
-                .statement_masks
-                .clone()
-                .map(|statement_mask| statement_mask.value()),
-            commitment_randomness: self.commitment_randomness,
-        };
+        let commitments = process_incoming_messages(self.party_id, self.provers.clone(), commitments)?;
 
         let proof_share_round_party =
             proof_share_round::Party::<REPETITIONS, Language, ProtocolContext> {
                 party_id: self.party_id,
-                threshold: self.threshold,
-                number_of_parties: self.number_of_parties,
+                provers: self.provers,
                 language_public_parameters: self.language_public_parameters,
                 protocol_context: self.protocol_context,
                 witnesses: self.witnesses,
@@ -100,6 +77,6 @@ for Party<REPETITIONS, Language, ProtocolContext>
                 commitments,
             };
 
-        Ok((decommitment, proof_share_round_party))
+        Ok((self.decommitment, proof_share_round_party))
     }
 }
