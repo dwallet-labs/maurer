ce54c3924b2d1a05c768c7cab9bb219be48f4490
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-13 11:34:19 +0200

    update

--- src/aggregation.rs
+++ src/aggregation.rs
@@ -1,11 +1,11 @@
 // Author: dWallet Labs, Ltd.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use group::PartyID;
 
-use crate::{language, Proof, Result};
+use crate::{language, Proof};
 
 pub mod commitment_round;
 pub mod decommitment_round;
@@ -16,40 +16,6 @@ pub type Output<const REPETITIONS: usize, Language, ProtocolContext> = (
     Proof<REPETITIONS, Language, ProtocolContext>,
     Vec<language::StatementSpaceGroupElement<REPETITIONS, Language>>,
 );
-
-fn process_incoming_messages<T>(
-    party_id: PartyID,
-    provers: HashSet<PartyID>,
-    messages: HashMap<PartyID, T>,
-) -> Result<HashMap<PartyID, T>> {
-    // First remove parties that didn't participate in the previous round, as they shouldn't be
-    // allowed to join the session half-way, and we can self-heal this malicious behaviour
-    // without needing to stop the session and report.
-    let messages: HashMap<PartyID, _> = messages
-        .into_iter()
-        .filter(|(pid, _)| *pid != party_id)
-        .filter(|(pid, _)| provers.contains(pid))
-        .collect();
-
-    let current_round_party_ids: HashSet<PartyID> = messages.keys().copied().collect();
-
-    let other_provers: HashSet<_> = provers.into_iter().filter(|pid| *pid != party_id).collect();
-
-    let mut unresponsive_parties: Vec<PartyID> = other_provers
-        .difference(&current_round_party_ids)
-        .cloned()
-        .collect();
-
-    unresponsive_parties.sort();
-
-    if !unresponsive_parties.is_empty() {
-        return Err(proof::aggregation::Error::UnresponsiveParties(
-            unresponsive_parties,
-        ))?;
-    }
-
-    Ok(messages)
-}
 
 #[cfg(feature = "test_helpers")]
 pub(super) mod test_helpers {
@@ -129,7 +95,7 @@ pub(super) mod test_helpers {
 
         assert!(
             proof
-                .verify(&PhantomData, &language_public_parameters, statements)
+                .verify(&PhantomData, language_public_parameters, statements)
                 .is_ok(),
             "valid aggregated proofs should verify"
         );
