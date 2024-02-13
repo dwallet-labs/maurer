fb10f84c8bbc320dad3407c411069d354244feab
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-13 11:44:47 +0200

    update

--- src/aggregation.rs
+++ src/aggregation.rs
@@ -19,10 +19,12 @@ pub type Output<const REPETITIONS: usize, Language, ProtocolContext> = (
 
 #[cfg(feature = "test_helpers")]
 pub(super) mod test_helpers {
+    use criterion::measurement::{Measurement, WallTime};
     use rand_core::OsRng;
     use std::collections::HashMap;
     use std::iter;
     use std::marker::PhantomData;
+    use std::time::Duration;
 
     use crate::test_helpers::sample_witnesses;
     use crate::Language;
@@ -52,7 +54,12 @@ pub(super) mod test_helpers {
         language_public_parameters: &Lang::PublicParameters,
         number_of_parties: usize,
         batch_size: usize,
-    ) -> HashMap<PartyID, commitment_round::Party<REPETITIONS, Lang, PhantomData<()>>> {
+    ) -> (
+        Duration,
+        HashMap<PartyID, commitment_round::Party<REPETITIONS, Lang, PhantomData<()>>>,
+    ) {
+        let measurement = WallTime;
+
         let witnesses = sample_witnesses_for_aggregation::<REPETITIONS, Lang>(
             language_public_parameters,
             number_of_parties,
@@ -61,25 +68,29 @@ pub(super) mod test_helpers {
         let number_of_parties = witnesses.len().try_into().unwrap();
         let provers = HashSet::from_iter(1..=number_of_parties);
 
-        witnesses
+        let mut instantiation_time = Duration::default();
+
+        let parties = witnesses
             .into_iter()
             .enumerate()
             .map(|(party_id, witnesses)| {
                 let party_id: u16 = (party_id + 1).try_into().unwrap();
-                (
+
+                let now = measurement.start();
+                let party = commitment_round::Party::new_session(
                     party_id,
-                    commitment_round::Party::new_session(
-                        party_id,
-                        provers.clone(),
-                        language_public_parameters.clone(),
-                        PhantomData,
-                        witnesses,
-                        &mut OsRng,
-                    )
-                    .unwrap(),
+                    provers.clone(),
+                    language_public_parameters.clone(),
+                    PhantomData,
+                    witnesses,
+                    &mut OsRng,
                 )
+                .unwrap();
+                instantiation_time = measurement.end(now);
+                (party_id, party)
             })
-            .collect()
+            .collect();
+        (instantiation_time, parties)
     }
 
     pub fn aggregates<const REPETITIONS: usize, Lang: Language<REPETITIONS>>(
@@ -87,7 +98,7 @@ pub(super) mod test_helpers {
         number_of_parties: usize,
         batch_size: usize,
     ) {
-        let commitment_round_parties =
+        let (_, commitment_round_parties) =
             setup::<REPETITIONS, Lang>(language_public_parameters, number_of_parties, batch_size);
 
         let (_, _, _, _, _, (proof, statements)) =
@@ -109,7 +120,7 @@ pub(super) mod test_helpers {
         number_of_parties: usize,
         batch_size: usize,
     ) {
-        let commitment_round_parties =
+        let (_, commitment_round_parties) =
             setup::<REPETITIONS, Lang>(language_public_parameters, number_of_parties, batch_size);
 
         proof::aggregation::test_helpers::unresponsive_parties_aborts_session_identifiably(
@@ -125,7 +136,7 @@ pub(super) mod test_helpers {
         number_of_parties: usize,
         batch_size: usize,
     ) {
-        let commitment_round_parties =
+        let (_, commitment_round_parties) =
             setup::<REPETITIONS, Lang>(language_public_parameters, number_of_parties, batch_size);
 
         proof::aggregation::test_helpers::wrong_decommitment_aborts_session_identifiably(
@@ -141,9 +152,9 @@ pub(super) mod test_helpers {
         number_of_parties: usize,
         batch_size: usize,
     ) {
-        let commitment_round_parties =
+        let (_, commitment_round_parties) =
             setup::<REPETITIONS, Lang>(language_public_parameters, number_of_parties, batch_size);
-        let wrong_commitment_round_parties =
+        let (_, wrong_commitment_round_parties) =
             setup::<REPETITIONS, Lang>(language_public_parameters, number_of_parties, batch_size);
 
         proof::aggregation::test_helpers::failed_proof_share_verification_aborts_session_identifiably(commitment_round_parties, wrong_commitment_round_parties);
@@ -153,15 +164,20 @@ pub(super) mod test_helpers {
         language_public_parameters: &Lang::PublicParameters,
         extra_description: Option<String>,
         as_millis: bool,
+        batch_sizes: Option<Vec<usize>>,
     ) {
         let timestamp = if as_millis { "ms" } else { "µs" };
         println!(
-            "\nLanguage Name, Repetitions, Extra Description, Number of Parties, Batch Size, Commitment Round Time ({timestamp}), Decommitment Round Time ({timestamp}), Proof Share Round Time ({timestamp}), Proof Aggregation Round Time ({timestamp}), Protocol Time ({timestamp})",
+            "\nLanguage Name, Repetitions, Extra Description, Number of Parties, Batch Size, Instantiation Time ({timestamp}), Commitment Round Time ({timestamp}), Decommitment Round Time ({timestamp}), Proof Share Round Time ({timestamp}), Proof Aggregation Round Time ({timestamp}), Protocol Time ({timestamp})",
         );
 
-        for number_of_parties in [10, 100, 1000] {
-            for batch_size in [1, 10, 100, 1000, 10000] {
-                let commitment_round_parties = setup::<REPETITIONS, Lang>(
+        for number_of_parties in [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024] {
+            for batch_size in batch_sizes
+                .clone()
+                .unwrap_or(vec![1, 2, 4, 8, 16, 32, 64, 128])
+                .into_iter()
+            {
+                let (instantiation_time, commitment_round_parties) = setup::<REPETITIONS, Lang>(
                     language_public_parameters,
                     number_of_parties,
                     batch_size,
@@ -176,36 +192,20 @@ pub(super) mod test_helpers {
                     _,
                 ) = proof::aggregation::test_helpers::aggregates(commitment_round_parties);
 
+                let measurement = WallTime;
+                let total_time = measurement.add(&total_time, &instantiation_time);
+
                 println!(
-                    "{}, {}, {}, {number_of_parties}, {batch_size}, {:?}, {:?}, {:?}, {:?}, {:?}",
+                    "{}, {}, {}, {number_of_parties}, {batch_size}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}",
                     Lang::NAME,
                     REPETITIONS,
                     extra_description.clone().unwrap_or("".to_string()),
-                    if as_millis {
-                        commitment_round_time.as_millis()
-                    } else {
-                        commitment_round_time.as_micros()
-                    },
-                    if as_millis {
-                        decommitment_round_time.as_millis()
-                    } else {
-                        decommitment_round_time.as_micros()
-                    },
-                    if as_millis {
-                        proof_share_round_time.as_millis()
-                    } else {
-                        proof_share_round_time.as_micros()
-                    },
-                    if as_millis {
-                        proof_aggregation_round_time.as_millis()
-                    } else {
-                        proof_aggregation_round_time.as_micros()
-                    },
-                    if as_millis {
-                        total_time.as_millis()
-                    } else {
-                        total_time.as_micros()
-                    },
+                    if as_millis { instantiation_time.as_millis() } else { instantiation_time.as_micros() },
+                    if as_millis { commitment_round_time.as_millis() } else { commitment_round_time.as_micros() },
+                    if as_millis { decommitment_round_time.as_millis() } else { decommitment_round_time.as_micros() },
+                    if as_millis { proof_share_round_time.as_millis() } else { proof_share_round_time.as_micros() },
+                    if as_millis { proof_aggregation_round_time.as_millis() } else { proof_aggregation_round_time.as_micros() },
+                    if as_millis { total_time.as_millis() } else { total_time.as_micros() },
                 );
             }
         }
